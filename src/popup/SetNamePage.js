36cea65f0213b206b389675ff738a924a557a62f
Alan Shaw <alan.shaw@protocol.ai>
2019-10-02 06:43:36 +0100

    feat: styles for st name page

--- src/popup/SetNamePage.js
+++ src/popup/SetNamePage.js
@@ -24,11 +24,30 @@ class SetNamePage extends Component {
   render () {
     const { name } = this.state
     return (
-      <form onSubmit={() => { this.handleSubmit(); return false }}>
-        <label htmlFor='name'>Name</label>
-        <input onInput={e => this.setState({ name: e.target.value })} value={name} />
-        <button type='submit'>Submit</button>
-      </form>
+      <div className='flex items-center justify-center h-100'>
+        <div className='tc'>
+          <div className='mb3'>
+            <img className='pr3 v-mid' src='images/icon.svg' style={{ width: '50px' }} />
+            <span className='montserrat fw6 f3 charcoal ttu v-mid'>Chatterbox</span>
+          </div>
+          <form onSubmit={() => { this.handleSubmit(); return false }}>
+            <input
+              className='input-reset charcoal ba b--black-20 br1 pa2 mb2 db w-75 center focus-outline'
+              onInput={e => this.setState({ name: e.target.value })}
+              value={name}
+              placeholder='Your name'
+            />
+            <button
+              type='submit'
+              className={`transition-all sans-serif dib fw5 lh-copy bn br1 pa2 pointer focus-outline white bg-${name ? 'green' : 'gray'} white ma2`}
+              style={{ minWidth: '140px' }}
+              disabled={!name}
+            >
+              Save
+            </button>
+          </form>
+        </div>
+      </div>
     )
   }
 }
