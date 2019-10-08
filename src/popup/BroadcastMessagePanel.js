97ba2c1c9ca0a233271541cc203a5ca31cd7ae66
Alan Shaw <alan.shaw@protocol.ai>
2019-10-08 11:39:59 +0100

    feat: show own peer ID resolves #5

--- src/popup/BroadcastMessagePanel.js
+++ src/popup/BroadcastMessagePanel.js
@@ -36,10 +36,14 @@ export class BroadcastMessagePanel extends Component {
             <Identicon string={peerInfo.id} size={75} />
           </div>
           <div className='montserrat fw6 f4 charcoal ttu mb3'>{peerInfo.name} <span className='fw4'>says:</span></div>
-          <form onSubmit={this.handleSubmit}>
+          <form onSubmit={this.handleSubmit} className='mb3'>
             <textarea onInput={this.handleInput} value={text} className='input-reset charcoal ba b--black-20 br3 pa2 mb2 focus-outline v-btm' onKeyDown={this.handleKeyDown} />
             <button type='submit' className={`transition-all sans-serif dib fw5 lh-copy bn br1 pv2 ph3 pointer focus-outline white bg-${text ? 'green' : 'gray'} white ma2`} disabled={!text.trim()}>Send</button>
           </form>
+          <div className='montserrat fw4 f7 charcoal'>
+            <span className='charcoal-muted mr1'>My Peer ID:</span>
+            {peerInfo.id}
+          </div>
         </div>
       </div>
     )
